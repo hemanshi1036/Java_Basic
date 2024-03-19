f2d723aa6aea96fd8a6cab1a27e0a64da9803d46
hemanshi1036 <hemanshisolanki2004@gmail.com>
2024-03-19 23:42:44 +0530

    matrix

---
+++ area.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class AreaCalculate{
+
+    public double area(double side){
+        return side * side;
+    }
+    public double area(double length, double width){
+        return length * width;
+    }
+    public double area(double height, int base){
+        return 0.5 * height * base;
+    }
+}
+public class area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        AreaCalculate calculate = new AreaCalculate();
+        double area = 0.0;
+        System.out.println("Enter The Shape what you find the Area : Square, Rectangle, Triangle " );
+        String Shape = sc.nextLine();
+        if(Shape.equalsIgnoreCase("Square")){
+            System.out.println("Enter the side of square : ");
+            double side = sc.nextDouble();
+            area = calculate.area(side);
+        }
+        else if(Shape.equalsIgnoreCase("Rectangle")){
+            System.out.println("Enter the length of Rectangle : ");
+            double length = sc.nextDouble();
+            System.out.println("Enter the width of Rectangle : ");
+            double width = sc.nextDouble();
+
+            area = calculate.area(length, width);
+        }
+        else if(Shape.equalsIgnoreCase("Triangle")){
+            System.out.println("Enter the base of Triangle : ");
+            double base = sc.nextDouble();
+            System.out.println("Enter the height of Triangle : ");
+            double height = sc.nextDouble();
+
+            area = calculate.area(base, height);
+        }
+        else {
+            System.out.println("This is not a mentioned Shape.");
+        }
+        System.out.println("The area of " + Shape + " is : " +area);
+    }
+}
---
+++ matrix.java
@@ -0,0 +1,55 @@
+import java.util.RandomAccess;
+class matrix{
+	public static void main(String[] args){
+		
+	Random r = new Random();
+	
+	int [][] matrix = new int[6][6];
+		
+
+		for(int i = 0; i<6; i++){
+		int count = 0;
+			for(int j = 0; j<6; j++){
+			matrix [i][j] = r.nextInt();
+			if(matrix [i][j] % 2 == 0){
+				System.out.println("0");
+			}
+			else{
+				System.out.println("1");
+				count++;
+				}
+
+			}
+		
+			System.out.println("");
+			if(count%2=0){
+				System.out.println("Number of '1' is in row even" );
+			}
+			else{
+				System.out.println("Number of '1' is in row odd");
+				}
+	
+		}
+
+				for(int j = 0; j<6; j++){
+				int count1 = 0;
+					for(int i = 0; i<6; i++){
+			if(matrix [i][j] % 2 != 0){
+		
+				count1++;
+				}
+
+}
+		System.out.println("");
+			if(count%2==0){
+				System.out.println("Number of '1' in column is even" );
+			}
+			else{
+				System.out.println("Number of '1' in column  is odd");
+				}
+	
+		}
+						  
+	
+}
+}  
