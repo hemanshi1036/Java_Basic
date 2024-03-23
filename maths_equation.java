255dc7708cba4c01e7ed93517e6504fc49ec5903
hemanshi1036 <hemanshisolanki2004@gmail.com>
2024-03-23 22:45:13 +0530

    simple_equation

---
+++ maths_equation.java
@@ -0,0 +1,19 @@
+public class equation {
+    public static void main(String[] args){
+        float a = 3.4f;
+        float b = 50.2f;
+        float e = 44.5f;
+        float c = 2.1f;
+        float d = 0.55f;
+        float f = 5.9f;
+
+        float x = ((e * d) - (b * f)) /  ((a * d) - (b * c));
+        float y = ((a * f) - (e * c)) / ((a * d) - (b * c));
+
+        System.out.println("The Value of x is : "+ x);
+        System.out.println("The Value of y is : "+ y);
+
+
+    }
+}
+
