85a16403ed83f11138681cd22c715f38ef880a06
hemanshi1036 <hemanshisolanki2004@gmail.com>
2024-04-25 21:35:00 +0530

    practiceset3

---
+++ practiceset3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class practiceset3 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Que 1
+        // String name = "HELLO";
+        // System.out.println(name.toLowerCase());
+
+        //QUE 2
+        // String name = "HELLO HEMANSHI HOW ARE YOU?";
+        // System.out.println(name.replace(" ", "_"));
+
+        //QUE 3
+        // String letter = "Dear <|name|>, Tnans a lot";
+        // System.out.println(letter.replace("<|name|>", "Hetanshi"));
+
+        //QUE 5
+        String letter = "Dear Harry ,\n This \'Java\' \t Course is nice. \\Thanks";
+        System.out.println(letter);
+
+
+    }
+
+}
