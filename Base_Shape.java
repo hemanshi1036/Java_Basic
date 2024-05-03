b0e1761cd6257f16263c9627a8f479b10ad1b0f1
hemanshi1036 <hemanshisolanki2004@gmail.com>
2024-05-03 21:40:17 +0530

    Shape

---
+++ Base_Shape.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Shape {
+    protected double width;
+    protected double height;
+    public void getData() {
+        Scanner scanner = new Scanner(System.in);
+System.out.println("Enter Values :");
+System.out.print("Width : ");
+        width = scanner.nextDouble();
+System.out.print("Height : ");
+        height = scanner.nextDouble();
+    }
+}
+class Triangle extends Shape {
+    public void displayArea() {
+        double area = 0.5 * width * height;
+System.out.println("Area of Triangle: " + area);
+    }
+}
+class Rectangle extends Shape {
+    public void displayArea() {
+        double area = width * height;
+System.out.println("Area of Rectangle: " + area);
+    }
+}
+public class Base_Shape {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        Rectangle rectangle = new Rectangle();
+System.out.println("Enter Triangle dimensions:");
+triangle.getData();
+triangle.displayArea();
+System.out.println("\nEnter Rectangle dimensions:");
+rectangle.getData();
+rectangle.displayArea();
+    }
+}
